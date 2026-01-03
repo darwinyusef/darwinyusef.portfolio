95d028f5dcc0c2cddea5e0b0d5370ba448b647b3
aquicreamos <wsgestor@gmail.com>
2026-01-03 18:06:27 -0500

    modificacion de docker y puerto

--- astro-portfolio/astro.config.mjs
+++ astro-portfolio/astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   prerender: true,
-
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [mdx()],
 
   i18n: {
